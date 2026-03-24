b366cde31cba39e157d8f6a14486cc07d0047893
sakirkilic <skrklcc@gmail.com>
2026-03-24 17:07:12 +0300

    feat: global exception middleware test edildi ve hata akışı doğrulandı

---
+++ src/Modules/Product/Product.API/Middlewares/ExceptionMiddleware.cs
@@ -0,0 +1,48 @@
+﻿using System.Net;
+using System.Text.Json;
+
+namespace Product.API.Middlewares
+{
+    // Tüm exception'ları yakalayan middleware
+    public sealed class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ExceptionMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        // Pipeline içinde çalışır
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                await HandleException(context, ex);
+            }
+        }
+
+        // Exception'ı HTTP response'a çevirir
+        private static async Task HandleException(HttpContext context, Exception exception)
+        {
+            context.Response.ContentType = "application/json";
+
+            var statusCode = HttpStatusCode.InternalServerError;
+
+            var response = new
+            {
+                message = exception.Message
+            };
+
+            context.Response.StatusCode = (int)statusCode;
+
+            var json = JsonSerializer.Serialize(response);
+
+            await context.Response.WriteAsync(json);
+        }
+    }
+}
